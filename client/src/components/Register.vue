8cdcfaca6fc0266c89c1cc61f35c6d02f8a73eb1
Alovuddinkhuja Shoyusupov <Alovuddinkhuja.Shoyusupov@ucell.uz>
2018-11-06 23:02:44 +0500

    authentication controller and policy have added

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,23 +1,37 @@
 <template>
-  <div>
-      <h1>Register</h1>
-      <input
-        type="text"
-        name="email"
-        v-model="email"
-        placeholder="email" />
-    <br>
-      <input
-        type="password"
-        name="password"
-        v-model="password"
-        placeholder="password" />
-    <br>
-    <button
-        @click="register">
-        Register
-    </button>
-  </div>
+  <v-container grid-list-xl text-xs-center>
+    <v-layout column>
+      <v-flex xs10 offset-xs1>
+        <div class="white elevation-2">
+          <v-toolbar flat dense class="purple darken-3" dark>
+            <v-toolbar-title>Register</v-toolbar-title>
+          </v-toolbar>
+          <div class="pl-4 pr-4 pt-2 pb-2">
+              <h1>Register</h1>
+              <input
+                type="text"
+                name="email"
+                v-model="email"
+                placeholder="email" />
+            <br>
+              <input
+                type="password"
+                name="password"
+                v-model="password"
+                placeholder="password" />
+            <br>
+            <div class="error" v-html="error" />
+            <br>
+            <v-btn
+                class="purple darken-3"
+                @click="register">
+                Register
+            </v-btn>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -25,21 +39,28 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
-      email: 'abc',
-      password: '123'
+      email: '',
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .error {
+    color:red;
+  }
 </style>
